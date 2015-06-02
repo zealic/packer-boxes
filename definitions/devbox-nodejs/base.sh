9b09caf36f6a85a8ac95f0630e79b04ccabb5d11
zealic <zealic@gmail.com>
2015-06-02 13:10:40 +0800

    * Auto install.

--- definitions/devbox-nodejs/base.sh
+++ definitions/devbox-nodejs/base.sh
@@ -5,12 +5,12 @@ sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 cat > /etc/yum.repos.d/epel.repo << EOM
 [epel]
 name=epel
-baseurl=http://download.fedoraproject.org/pub/epel/beta/7/\$basearch
+baseurl=http://mirrors.ustc.edu.cn/epel/7/\$basearch
 enabled=1
 gpgcheck=0
 EOM
 
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils
+yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils bzip2
 
 # Make ssh faster by not waiting on DNS
 echo "UseDNS no" >> /etc/ssh/sshd_config
