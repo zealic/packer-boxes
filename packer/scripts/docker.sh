5275b5bf9dd488077a20465e9fd7d9eab632a898
zealic <zealic@gmail.com>
2015-06-11 16:52:08 +0800

    + Added busybox, haproxy, etcd to docker.

--- packer/scripts/docker.sh
+++ packer/scripts/docker.sh
@@ -3,15 +3,18 @@ yum install -y docker
 sed -i 's|OPTIONS=|OPTIONS=--registry-mirror=https://docker.mirrors.ustc.edu.cn |g' /etc/sysconfig/docker
 systemctl enable docker
 systemctl start docker
-curl -sSL -o /usr/bin/weave https://github.com/weaveworks/weave/releases/download/v0.11.0/weave
+curl -sSL -o /usr/bin/weave https://github.com/weaveworks/weave/releases/download/v0.11.1/weave
 chmod a+x /usr/bin/weave
 weave setup
 
 PACKAGES=(
+  busybox
+  haproxy
   mongo
   nginx
   postgres
   redis
+  microbox/etcd
 )
 
 for pkg in ${PACKAGES[@]}
