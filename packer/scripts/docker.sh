75b0d0dd38ec88b616e1a6f01178502f9ec47e95
zealic <zealic@gmail.com>
2015-06-12 16:57:08 +0800

    * Improve docker install script.

--- packer/scripts/docker.sh
+++ packer/scripts/docker.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 yum install -y docker
+
+# Set mirror
+sed -i 's|OPTIONS=|OPTIONS=--registry-mirror=https://docker.mirrors.ustc.edu.cn |g' /etc/sysconfig/docker
+
+# Enable docker
 systemctl enable docker
 systemctl start docker
+
+# Install docker
 curl -sSL -o /usr/bin/weave https://github.com/weaveworks/weave/releases/download/v0.11.1/weave
 chmod a+x /usr/bin/weave
 weave setup
 
+# Install docker images
 PACKAGES=(
   busybox
   haproxy
@@ -20,8 +28,4 @@ for pkg in ${PACKAGES[@]}
 do
   docker pull $pkg
 done
-
-
-# Set mirror
-sed -i 's|OPTIONS=|OPTIONS=--registry-mirror=https://docker.mirrors.ustc.edu.cn |g' /etc/sysconfig/docker
 
