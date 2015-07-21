784588d3e25bf7c9d80b118980831df8509b742b
zealic <zealic@gmail.com>
2015-07-21 15:39:59 +0800

    * Use USTC CentOS and EPEL mirrors.

--- packer/scripts/packages.sh
+++ packer/scripts/packages.sh
@@ -9,14 +9,14 @@ EOF
 sed -i 's|enabled=1|enabled=0|g' /etc/yum/pluginconf.d/fastestmirror.conf
 
 # Setup yum
-sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://mirrors.cat.pdx.edu|g' /etc/yum.repos.d/CentOS-Base.repo
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://mirrors.ustc.edu.cn|g' /etc/yum.repos.d/CentOS-Base.repo
 sed -i 's|mirrorlist=|#mirrorlist=|g' /etc/yum.repos.d/CentOS-Base.repo
 
 # Setup EPEL
 yum install -y epel-release
-sed -i 's|#baseurl=http://download.fedoraproject.org/pub|baseurl=http://mirrors.cat.pdx.edu|g' /etc/yum.repos.d/epel.repo
+sed -i 's|#baseurl=http://download.fedoraproject.org/pub|baseurl=http://mirrors.ustc.edu.cn|g' /etc/yum.repos.d/epel.repo
 sed -i 's|mirrorlist=|#mirrorlist=|g' /etc/yum.repos.d/epel.repo
-sed -i 's|#baseurl=http://download.fedoraproject.org/pub|baseurl=http://mirrors.cat.pdx.edu|g' /etc/yum.repos.d/epel-testing.repo
+sed -i 's|#baseurl=http://download.fedoraproject.org/pub|baseurl=http://mirrors.ustc.edu.cn|g' /etc/yum.repos.d/epel-testing.repo
 sed -i 's|mirrorlist=|#mirrorlist=|g' /etc/yum.repos.d/epel-testing.repo
 
 # Utils
