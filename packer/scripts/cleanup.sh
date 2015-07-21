8fb532ccf32b75f2ab72b66018537cce5ef202c6
zealic <zealic@gmail.com>
2015-07-21 15:33:05 +0800

    * Python pip mirrors support.

--- packer/scripts/cleanup.sh
+++ packer/scripts/cleanup.sh
@@ -14,8 +14,6 @@ find /var/log -type f | while read f; do echo -ne '' > $f; done;
 rm -rf /tmp/*
 
 rm -f /etc/udev/rules.d/70-persistent-net.rules
-# See also: https://github.com/mitchellh/vagrant/issues/2614
-ln -sf /dev/null /etc/udev/rules.d/70-persistent-net.rules
 sed -i '/^UUID/d'   /etc/sysconfig/network-scripts/ifcfg-enp0s3
 sed -i '/^HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-enp0s3
 
--- packer/scripts/lang-python.sh
+++ packer/scripts/lang-python.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 yum install -y python python-devel python-pip
 pip install distribute virtualenv nose 
+
+mkdir -p /root/.pip
+cat > /root/.pip/pip.conf <<EOF
+[global]
+index-url = https://pypi.mirrors.ustc.edu.cn/simple
+EOF
+
+cp /home/vagrant/.pip/pip.conf /home/vagrant/.pip/pip.conf
+chown vagrant:vagrant /home/vagrant/.pip/pip.conf
