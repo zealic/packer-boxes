7adf1c88aaafc6fb0b10820fdc15025f9b68c63e
zealic <zealic@gmail.com>
2015-08-12 15:29:31 +0800

    * Update docker to 1.8.0.

--- packer/scripts/docker.sh
+++ packer/scripts/docker.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
-DOCKER_VERSION=1.7.1
-curl -o /tmp/docker.rpm -sSL https://get.docker.com/rpm/$DOCKER_VERSION/centos-7/RPMS/x86_64/docker-engine-$DOCKER_VERSION-1.el7.centos.x86_64.rpm
+DOCKER_VERSION=1.8.0
+curl -o /tmp/docker.rpm -sSL http://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-$DOCKER_VERSION-1.el7.centos.x86_64.rpm
 yum localinstall -y --nogpgcheck /tmp/docker.rpm
-
-# Docker configuration
-mkdir -p /etc/systemd/system/docker.service.d
-cat > /etc/systemd/system/docker.service.d/docker.conf <<'EOF'
-[Service]
-EnvironmentFile=-/etc/default/docker
-ExecStart=
-ExecStart=/usr/bin/docker -d -H fd:// $DOCKER_OPTS
-EOF
-cat > /etc/default/docker <<'EOF'
-DOCKER_OPTS="--registry-mirror=https://docker.mirrors.ustc.edu.cn"
-EOF
 
 # Enable docker
 systemctl enable docker
