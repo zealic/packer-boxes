58c219eb4cbec7d5ba6c93e0a0ad75ea8cd58d6b
zealic <zealic@gmail.com>
2015-09-16 15:52:16 +0800

    * Remove weave and docker images.

--- packer/scripts/docker.sh
+++ packer/scripts/docker.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Install docker
 DOCKER_VERSION=1.8.2
 curl -o /tmp/docker.rpm -sSL http://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-$DOCKER_VERSION-1.el7.centos.x86_64.rpm
 yum localinstall -y --nogpgcheck /tmp/docker.rpm
@@ -6,30 +7,3 @@ yum localinstall -y --nogpgcheck /tmp/docker.rpm
 # Enable docker
 systemctl enable docker
 systemctl start docker
-
-# Install docker
-WEAVE_VERSION=1.1.0
-curl -sSL -o /usr/bin/weave https://github.com/weaveworks/weave/releases/download/v$WEAVE_VERSION/weave
-chmod a+x /usr/bin/weave
-weave setup
-
-# Install docker images
-PACKAGES=(
-  busybox
-  haproxy
-  mongo
-  nginx
-  postgres
-  redis
-
-  # Langugages
-  node
-  python:2.7
-  python:latest
-  ruby
-)
-
-for pkg in ${PACKAGES[@]}
-do
-  docker pull $pkg
-done
