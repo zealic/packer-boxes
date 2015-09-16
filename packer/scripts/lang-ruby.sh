681c66dac79583b14c1304fd0f7a31c647696701
zealic <zealic@gmail.com>
2015-09-16 16:48:49 +0800

    * Update ruby script.

--- packer/scripts/lang-ruby.sh
+++ packer/scripts/lang-ruby.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-yum install -y ruby ruby-devel
-gem install rake bundler
 gem sources --remove https://rubygems.org/
 gem sources -a https://ruby.taobao.org/
+yum install -y ruby ruby-devel
+gem install rake bundler
