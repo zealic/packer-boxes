fd30fe95712d88540a7d11be0a246425038421c6
zealic <zealic@gmail.com>
2015-09-18 18:51:45 +0800

    * Normalize cleanup scripts.

--- packer/scripts/cleanup.sh
+++ packer/scripts/cleanup.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
+##########################################################
+# General
+##########################################################
 # Remove Linux headers
 yum -y clean all
-
-# Remove Virtualbox specific files
-rm -rf /usr/src/vboxguest* /usr/src/virtualbox-ose-guest*
-rm -rf *.iso *.iso.? /tmp/vbox /home/vagrant/.vbox_version
 
 # Cleanup log files
 find /var/log -type f | while read f; do echo -ne '' > $f; done;
@@ -12,9 +11,22 @@ find /var/log -type f | while read f; do echo -ne '' > $f; done;
 # remove under tmp directory
 rm -rf /tmp/*
 
+# Remove kickstart configuration
+rm /root/anaconda-ks.cfg
+
+# Clean network
 rm -f /etc/udev/rules.d/70-persistent-net.rules
 sed -i '/^UUID/d'   /etc/sysconfig/network-scripts/ifcfg-enp0s3
 sed -i '/^HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-enp0s3
 
+
+##########################################################
+# Other
+##########################################################
+# Remove Virtualbox specific files
+rm -rf /usr/src/vboxguest* /usr/src/virtualbox-ose-guest*
+rm -rf *.iso *.iso.? /tmp/vbox /home/vagrant/.vbox_version
+
+# Cleanup disk
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -rf /EMPTY
--- vmlord/scripts/cleanup.sh
+++ vmlord/scripts/cleanup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+##########################################################
+# General
+##########################################################
 # Remove Linux headers
 yum -y clean all
 
@@ -8,9 +11,17 @@ find /var/log -type f | while read f; do echo -ne '' > $f; done;
 # remove under tmp directory
 rm -rf /tmp/*
 
+# Remove kickstart configuration
+rm /root/anaconda-ks.cfg
+
+# Clean network
 rm -f /etc/udev/rules.d/70-persistent-net.rules
 sed -i '/^UUID/d'   /etc/sysconfig/network-scripts/ifcfg-enp0s3
 sed -i '/^HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-enp0s3
 
+
+##########################################################
+# Other
+##########################################################
 # Remove root permit permission on ssh
 sed -i -E '/^Permit.+/d' /etc/ssh/sshd_config
