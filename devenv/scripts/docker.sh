25762ee5ba3c0180e695486420ec09bacf517263
zealic <zealic@gmail.com>
2015-10-09 17:50:10 +0800

    * Add vagrant user to docker group.

--- devenv/scripts/docker.sh
+++ devenv/scripts/docker.sh
@@ -4,6 +4,12 @@ DOCKER_VERSION=1.8.2
 curl -o /tmp/docker.rpm -sSL http://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-$DOCKER_VERSION-1.el7.centos.x86_64.rpm
 yum localinstall -y --nogpgcheck /tmp/docker.rpm
 
+# Add vagrant user to docker group
+getent passwd vagrant >/dev/null 2>&1 && ret=true
+if $ret; then
+  usermod vagrant -a -G docker vagrant
+fi
+
 # Enable docker
 systemctl enable docker
 systemctl start docker
