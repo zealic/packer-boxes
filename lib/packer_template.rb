2d85dfcf369950581b4034aa264cfa81848271fc
zealic <zealic@gmail.com>
2015-11-03 20:04:41 +0800

    * Use spec.yml to generate palcer template.

--- lib/packer_template.rb
+++ lib/packer_template.rb
@@ -10,7 +10,7 @@ DEFAULT_PROVIDER = 'virtualbox'
 
 class PackerTemplate
   attr_reader :build_format, :manifest, :provider
-  attr_reader :file, :build_date, :packer_template, :builder_template
+  attr_reader :file, :build_date, :spec, :packer_template
 
   def initialize(build_format, manifest, provider, task_env = nil)
     defaults = CONFIG['defaults']
@@ -19,34 +19,30 @@ class PackerTemplate
     @provider = provider || defaults['provider']
     @file = File.join(get_basedir(), "packer-template.json")
     @build_date = DateTime.now.strftime("%Y%m%d")
+    @spec = YAML.load(IO.read("boxes/#{@manifest}/spec.yml"))
     FileUtils.mkdir_p(get_basedir())
 
     # Load packer template
-    File.open("boxes/#{@manifest}/template.json", "r" ) do |f|
-      @packer_template = JSON.load(f)
-    end
-    if @packer_template["builders"].count != 1 then
-      fail("Invalid template builders count, must be 1.") 
-    end
-    @builder_template = @packer_template['builders'][0].clone
-    @packer_template['builders'] = []
+    @packer_template = {
+      'variables': {
+        'memory_size':    "1024",
+        'cpu_count':      "1",
+        'atlas_user':     CONFIG['atlas_user'],
+        'version':        CONFIG['version'],
+        'template_name':  @manifest,
+        'ssh_username':   'root',
+        'ssh_password':   SecureRandom.base64,
+        'build_format':   @build_format,
+        'build_date':     @build_date,
+        'build_provider': @provider
+      },
+      'builders': []
+    }
 
     # Setup variables
-    @packer_template['variables'] = {
-      'memory_size':    "1024",
-      'cpu_count':      "1",
-      'atlas_user':     CONFIG['atlas_user'],
-      'version':        CONFIG['version'],
-      'template_name':  @manifest,
-      'ssh_username':   'root',
-      'ssh_password':   SecureRandom.base64,
-      'build_format':   @build_format,
-      'build_date':     @build_date,
-      'build_provider': @provider
-    }
     mf_vars = CONFIG["manifests"][@manifest]
-    variables = mf_vars[task_env] || mf_vars['_'].each do |k,v|
-      @packer_template['variables'][k] = v
+    (mf_vars[task_env] || mf_vars['_']).each do |k,v|
+      @packer_template[:variables][k.to_sym] = v
     end
   end
 
@@ -61,40 +57,63 @@ class PackerTemplate
     end
   end
 
-  def generate()
+  def generate(options={})
     # Generate packer template
     template = @packer_template.clone
-    template['builders'].push(make_builder(@build_format))
+    provider, builder = make_builder(@build_format)
+    template[:builders].push(builder)
 
-    # For vagrant only instruction
-    if @build_format != 'vagrant' then
-      template['builders'].push(make_builder('vagrant'))
+    # Generate provisioners
+    provisioners = []
+    shell_env = []
+    env_list = {
+      'BUILD_FORMAT': @build_format,
+      'BUILD_DATE': @build_date,
+      'BUILD_MANIFEST': @manifest,
+      'BUILD_PROVIDER': "{{user `build_provider`}}",
+      'BUILD_GUEST_OS': is_debian ? 'debian' : is_centos ? 'centos': 'other'
+    }
+    if is_debian then
+      env_list['DEBIAN_FRONTEND'] = "noninteractive"
     end
-
-    # Normalize provisioners
-    (template['provisioners'] || []).each do |p|
-      if p['type'] == 'shell' then
-        p['environment_vars'] = [] unless p['environment_vars']
-        env_list = {
-          'BUILD_FORMAT': @build_format,
-          'BUILD_DATE': @build_date,
-          'BUILD_MANIFEST': @manifest,
-          'BUILD_PROVIDER': "{{user `build_provider`}}",
-          'BUILD_GUEST_OS': is_debian ? 'debian' : is_centos ? 'centos': 'other'
-        }
-        if is_debian then
-          env_list['DEBIAN_FRONTEND'] = "noninteractive"
-        end
-        env_list.each do |k, v|
-          p['environment_vars'].push("#{k}=#{v}")
-        end
-      end
+    env_list.each do |k, v|
+      shell_env.push("#{k}=#{v}")
     end
+    (@spec['provisioners-shell'] || []).each do |p|
+      scripts = p.kind_of?(Array) ? p : [p]
+      provisioners.push({
+        'type': "shell",
+        'scripts': scripts,
+        'execute_command': "echo '{{user `ssh_password`}}' | {{.Vars}} sudo -S -E bash '{{.Path}}'",
+        'environment_vars': shell_env
+      })
+    end
+    template[:provisioners] = provisioners
 
     # Generate push
-    template['push'] = {
-      "exclude": [".*", "*.box", "output-*", "packer_cache", "*.json"]
-    }
+    if options[:push] then
+      template[:post-processors] = [[
+        {
+          "type": "vagrant",
+          "keep_input_artifact": false,
+          "only": ["vagrant"]
+        },
+        {
+          "type": "atlas",
+          "artifact": "{{user `atlas_user`}}/{{user `template_name`}}",
+          "artifact_type": "vagrant.box",
+          "metadata": {
+              "created_at": "{{timestamp}}",
+              "provider": provider,
+              "version": "{{user `version`}}"
+          },
+          "only": ["vagrant"]
+        }
+      ]]
+      template[:push] = {
+        "exclude": [".*", "*.box", "output-*", "packer_cache", "*.json"]
+      }
+    end
 
     # Generate template
     IO.binwrite(@file, JSON.pretty_generate(template))
@@ -102,7 +121,7 @@ class PackerTemplate
     # Generate ks template
     ks_template = IO.read("boxes/#{@manifest}/ks.cfg")
     Dir.chdir(get_basedir()) do
-      ks_scope = @packer_template['variables'].each_with_object({}){|(k,v), h| h[k.to_sym] = v}
+      ks_scope = @packer_template[:variables].each_with_object({}){|(k,v), h| h[k.to_sym] = v}
       # kickstart file not support CRLF newline
       IO.binwrite("ks.cfg", ks_template % ks_scope)
     end
@@ -114,7 +133,7 @@ class PackerTemplate
   end
 
   def push()
-    generate()
+    generate(push: true)
 
     Dir.chdir(get_basedir()) do
       FileUtils.rm_rf(Dir.glob("output-#{@name}"))
@@ -135,26 +154,40 @@ class PackerTemplate
   end
 
   private
-  def make_builders(name)
-    builder = @builder_template.clone
-    builder['name'] = name
-    template['builders'].push(builder)
-    return builder
-  end
-
-  private
   def make_builder(format)
-    builder = @builder_template.clone
-    builder['name'] = format
-    builder['vm_name'] = "{{user `template_name`}}"
-    builder['iso_url'] = "{{user `iso_url`}}"
-    builder['iso_checksum'] = "{{user `iso_checksum`}}"
-    builder['iso_checksum_type'] = "{{user `iso_checksum_type`}}"
-    builder['http_directory'] = "."
-    builder['ssh_wait_timeout'] = "60m"
-    builder['boot_wait'] = "5s"
-    builder['shutdown_command'] = "sudo -S /sbin/halt -h -p"
-    builder['disk_size'] = 10240
+    builder = {
+      'name': format,
+      'vm_name': "{{user `template_name`}}",
+      'iso_url': "{{user `iso_url`}}",
+      'iso_checksum': "{{user `iso_checksum`}}",
+      'iso_checksum_type': "{{user `iso_checksum_type`}}",
+      'http_directory': ".",
+      'ssh_username': "{{user `ssh_username`}}",
+      'ssh_password': "{{user `ssh_password`}}",
+      'ssh_wait_timeout': "60m",
+      'boot_wait': "5s",
+      'shutdown_command': "sudo -S /sbin/halt -h -p",
+      'disk_size': 10240
+    }
+
+    if is_centos then
+      builder['boot_command'] = [
+        "<tab> text ks=http://{{ .HTTPIP }}:{{ .HTTPPort }}/ks.cfg<enter>"
+      ]
+    elsif is_debian then
+      builder['boot_command'] = [
+        "<esc><wait>",
+        "install <wait>",
+        "preseed/url=http://{{ .HTTPIP }}:{{ .HTTPPort }}/ks.cfg <wait>",
+        "auto <wait>",
+        "debian-installer=en_US <wait>",
+        "locale=en_US <wait>",
+        "netcfg/get_hostname=localhost <wait>",
+        "netcfg/get_domain=localhost <wait>",
+        "keymap=us <wait>",
+        "<enter><wait>"
+      ]
+    end
 
     provider = @provider
     # Formats
@@ -180,7 +213,7 @@ class PackerTemplate
       builder['vboxmanage'] = [
         ["modifyvm", "{{.Name}}", "--memory", "{{user `memory_size`}}"],
         ["modifyvm", "{{.Name}}", "--cpus", "{{user `cpu_count`}}"],
-        # DNS Speed Ip
+        # DNS Speed UP
         ["modifyvm", "{{.Name}}", "--natdnshostresolver1", "on"],
         ["modifyvm", "{{.Name}}", "--natdnsproxy1", "on"]
       ]
@@ -209,10 +242,7 @@ class PackerTemplate
       fail("Not supported provider '#{provider}'.")
     end
 
-    builder['ssh_username'] = "{{user `ssh_username`}}"
-    builder['ssh_password'] = "{{user `ssh_password`}}"
-
-    return builder
+    return provider, builder
   end
 
   def normalize_manifest(manifest)
