8137f6d1b4b99cc59d001a54964654fed063b749
zealic <zealic@gmail.com>
2015-11-04 17:04:29 +0800

    * Update docker to 1.9.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 # Install docker
-DOCKER_VERSION=1.8.3
+DOCKER_VERSION=1.9.0
 
 if [[ $BUILD_GUEST_OS =~ centos ]]; then
-  curl -o /tmp/docker.rpm -sSL http://yum.dockerproject.org/repo/main/centos/7/Packages/docker-engine-$DOCKER_VERSION-1.el7.centos.x86_64.rpm
-  yum localinstall -y --nogpgcheck /tmp/docker.rpm
+  cat >/etc/yum.repos.d/docker.repo <<-EOF
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/7
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+  yum install docker-engine-${DOCKER_VERSION}
 elif [[ $BUILD_GUEST_OS =~ debian ]]; then
   apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
   echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list
   apt-get install -y apt-transport-https
   sleep 3; apt-get update -qq
-  apt-get install -y -qq docker-engine
+  apt-get install -y -qq docker-engine=${DOCKER_VERSION}\*
 fi
 
 
