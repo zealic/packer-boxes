371121aa8f3b71a928c663beb13c2314df286069
zealic <zealic@gmail.com>
2016-01-13 17:04:38 +0800

    * Fix alpine-linux install script.

--- manifests/alpine-3-devenv/http/install.sh
+++ manifests/alpine-3-devenv/http/install.sh
@@ -8,18 +8,27 @@ auto eth0
 iface eth0 inet dhcp
     hostname vagrant
 EOF
-setup-dns -d local 8.8.8.8
 setup-timezone -z UTC
 setup-apkrepos http://%{mirror_host}/alpine/v3.3/main
 setup-ntp -c chrony
+apk add --update
 apk add e2fsprogs syslinux mkinitfs
-mkfs.ext4 /dev/sda
-mount -t ext4 /dev/sda /mnt
+echo "n
+p
+1
+
+
+a
+1
+w
+" | fdisk /dev/sda
+mkfs.ext4 /dev/sda1
+mount -t ext4 /dev/sda1 /mnt
 setup-disk /mnt
-#setup-alpine -f ks.cfg <<EOF
-#%{ssh_password}
-#%{ssh_password}
-#y
-#EOF
+chroot /mnt /bin/ash -c "echo -e '%{ssh_password}\n%{ssh_password}' | passwd"
 chroot /mnt /bin/ash -c "apk add openssh && rc-update add sshd default"
+chroot /mnt /bin/ash -c "apk add sudo bash"
+sed -i -r 's/^#?(PermitRootLogin|PasswordAuthentication) .+/\1 yes/' /mnt/etc/ssh/sshd_config
+dd if=/usr/share/syslinux/mbr.bin of=/dev/sda
+extlinux --install /mnt/boot
 umount /mnt
