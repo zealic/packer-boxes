939028be85871cb37eac90f0a0899301421ecf05
zealic <zealic@gmail.com>
2016-02-19 11:43:35 +0800

    * Update npm to latest.

--- scripts/lang/nodejs.sh
+++ scripts/lang/nodejs.sh
@@ -4,7 +4,9 @@ BASE_VERSION=4.x
 if [[ $BUILD_GUEST_OS =~ centos ]]; then
   curl -sL https://rpm.nodesource.com/setup_$BASE_VERSION | bash -
   yum install -y nodejs
+  npm install -g npm
 elif [[ $BUILD_GUEST_OS =~ debian ]]; then
   curl -sL https://deb.nodesource.com/setup_$BASE_VERSION | bash -
   apt-get install -y -qq nodejs
+  npm install -g npm
 fi
