22edd4bac89ea1291702018cabb8b9928ff40890
zealic <zealic@gmail.com>
2016-02-19 11:22:20 +0800

    * Update golang to 1.6.

--- scripts/lang/golang.sh
+++ scripts/lang/golang.sh
@@ -5,7 +5,7 @@ elif [[ $BUILD_GUEST_OS =~ debian ]]; then
   apt-get install -y -qq git subversion
 fi
 
-GO_VERSION=1.5.3
+GO_VERSION=1.6
 curl -SL -o /tmp/golang.tar.gz https://storage.googleapis.com/golang/go$GO_VERSION.linux-amd64.tar.gz
 tar -C /usr/local -xzf /tmp/golang.tar.gz
 
